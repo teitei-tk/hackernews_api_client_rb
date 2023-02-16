8c760f3f31cf84d9f774d980b0e47112fb41d97a
teitei-tk <teitei.tk@gmail.com>
2023-02-16 21:25:29 +0900

    add faraday connection

---
+++ lib/hackernews_api_client/faraday/connection.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module HackernewsApiClient
+  module Faraday
+    module Connection
+      private
+
+      def connection
+        @connection ||=
+          begin
+            options = {
+              headers: {
+                'Accept' => 'application/json; charset=utf-8',
+                'User-Agent' => "HackerNews Ruby Client/#{HackernewsApiClient::VERSION}"
+              }
+            }
+
+            url = "https://hacker-news.firebaseio.com"
+            ::Faraday.new(url, options) do |connection|
+              connection.request :multipart
+              connection.request :url_encoded
+              connection.response :json, content_type: /\b*$/
+              connection.response :logger, HackernewsApiClient::Logger.default
+              connection.adapter ::Faraday.default_adapter
+            end
+          end
+      end
+    end
+  end
+end
